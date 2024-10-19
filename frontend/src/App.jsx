8ed38ba7bb0100840913d0f851f2ff8a5b046b95
Aditya kaushik <kaushikaditya985@gmail.com>
2024-10-19 15:14:20 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Doctors from "./pages/Doctors";
+import Appointments from "./pages/Doctors";
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:max-[10%]:">
+     <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointments/:docId" element={<Appointments />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
